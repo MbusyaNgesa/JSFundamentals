d6e30bcc19ae0cf373e7903a0b76cc4cb5e3e333
MbusyaNgesa <theengesa@gmail.com>
2024-06-13 12:25:34 +0300

    For and While Loops

--- script.js
+++ script.js
@@ -27,3 +27,108 @@ console.log(numbers);
 const fruits = ["Mangoes", "Oranges", "Apples"];
 
 console.log(fruits[1]);
+
+//Array 2
+let fruit = ["Mangoes", "Oranges", "Grapes"];
+fruit[3] = "Banana";
+console.log(fruit);
+
+//Push object to end of an Array
+
+let Ian = ["Ngesa", "Mbusya"];
+Ian.push("Dev");
+//Push Object to beginning of an array
+Ian.unshift("Rada");
+//Removing object
+Ian.pop();
+console.log(Ian);
+//If an object is an array (true or false)
+console.log(Array.isArray("hello"));
+//Indexing
+console.log(Ian.indexOf("Ngesa"));
+
+//Object literals
+const person = {
+  firstName: "John",
+  lastName: "Smith",
+  age: 28,
+  hobbies: ["music", "eating", "hiking", "hunting"],
+  address: {
+    street: "00100 Utawala",
+    city: "Nairobi",
+    state: "Avenue",
+  },
+};
+
+//Destructuring an array and embedded object
+const {
+  firstName,
+  lastName,
+  address: { state },
+} = person;
+
+console.log(person.firstName, person.lastName);
+console.log(person.hobbies[1]);
+console.log(firstName);
+console.log(state);
+
+//Creating objects
+const todos = [
+  {
+    id: 1,
+    text: "I am hungry",
+    isCompleted: true,
+    z: null,
+  },
+  {
+    id: 2,
+    text: "I just ate",
+    isCompleted: false,
+    z: null,
+  },
+  {
+    id: 3,
+    text: "I am Vengeace",
+    isCompleted: true,
+    z: null,
+  },
+];
+
+console.log(todos[2].text);
+
+//Sending data to Server
+const todosJSON = JSON.stringify(todos);
+console.log(todosJSON);
+
+//For Loops
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+//For Loop Decrement
+for (let j = 10; j > 1; j--) {
+  console.log(`Decrement: ${j}`);
+}
+
+//While loop
+let i = 0;
+while (i < 10) {
+  console.log(`While Loop Numbers: ${i}`);
+  i++;
+}
+//While Decrement
+/* let j = 10;
+while (j > 1) {
+  console.log(`Decrement Numbers:${j}`);
+  i--;
+}*/
+
+//Looping styles
+//Style 1
+for (let k = 0; k < todos.length; k++) {
+  console.log(todos[k].isCompleted);
+}
+
+//Style 2
+for (let to of todos) {
+  console.log(to.id);
+}
